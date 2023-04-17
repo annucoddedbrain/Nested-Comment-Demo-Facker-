9e938e8768a5a6b23748d2b861df116f2caa33a6
AzzadCodeBrain <azzad@coddedbrain.com>
2023-04-17 12:58:32 +0530

    upload fake data in post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'resume',
+        'banner_image',
+        'user_id'
+    ];
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this-> attributes ['slug'] = Str::slug($value);
+    }
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,7 +18,12 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->paragraph(1),
+            'description' => $this->faker->paragraph(4),
+            'resume' => $this->faker->paragraph(2),
+            'banner_image' => $this->faker->imageUrl($width=640,$height=480),
+            'user_id' => User::all()->random()->id
+
         ];
     }
 }
