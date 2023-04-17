b08f4b6108bc40e8c77a35573fa4a92c2471887a
AzzadCodeBrain <azzad@coddedbrain.com>
2023-04-17 13:07:07 +0530

    done post Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $post = Post::all();
+
+        return view('post.index',compact('posts'));
+    }
+
+    public function show($slug){
+        $post = Post::whereSlug($slug)->first();
+
+        return view('post.view', compact('posts'));
+    }
 }
