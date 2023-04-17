5ef7721ece2bf39a00b401d823ee25a0c2379847
AzzadCodeBrain <azzad@coddedbrain.com>
2023-04-17 12:41:59 +0530

    migrate post table

--- database/migrations/2023_04_17_065703_create_posts_table.php
+++ database/migrations/2023_04_17_065703_create_posts_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('slug');
+            $table->string('resume');
+            $table->string('banner_image')->nullable();
+            $table->foreignId('user_id')->constrained()->nullable();
             $table->timestamps();
         });
     }
